804789a4f1027ff09fb67de88ccad495360dcb46
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2020-05-10 13:08:35 +0200

    Question Component added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Question from './Question'
 
 class Dashboard extends Component {
     render()
-        {
-            console.log("Dashboard");
-            return (
-                <div>
-                    <h3 className="center">Your timeline</h3>
-                    <ul className='dashboard-list'>
-                        {this.props.questionIds.map((id) => (
-                            <li key={id}>
-                                <div>QUESTION ID: {id}</div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )
-        }}
+    {
+        return (
+            <div>
+                <h3 className="center">Your timeline</h3>
+                <ul className='dashboard-list'>
+                    {this.props.questionIds.map((id) => (
+                        <li key={id}>
+                            <Question id={id}/>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }}
 
 function mapStateToProps({ questions }) {
     return {
         questionIds: Object.keys(questions)
-          .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+            .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
     }
 }
 
- export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps)(Dashboard)
