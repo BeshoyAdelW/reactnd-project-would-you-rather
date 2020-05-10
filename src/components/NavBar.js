177d7973c15947f7a51141860e92d8d5dcefcec1
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2020-05-10 14:49:05 +0200

    Add LeaderBoard and save question

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,68 +7,71 @@ import React, { PureComponent, Fragment } from "react";
 import User from "./User"
 
 class NavBar extends PureComponent {
-  state = {
-    isOpen: false
-  };
+    state = {
+        isOpen: false
+    };
 
-  toggle = () =>  {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  };
+    toggle = () =>  {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    };
 
-  logout = (e) => {
-    const { logout} = this.props;
-    logout()
-  };
+    logout = (e) => {
+        const { logout} = this.props;
+        logout()
+    };
 
-  render() {
-    const { authedUser } = this.props;
+    render() {
+        const { authedUser } = this.props;
 
-    return (
-        <div>
-          <Navbar bg="primary" variant="dark" light expand="md">
-            <NavbarBrand tag={Link} to="/">Would You Rather</NavbarBrand>
-            { authedUser &&
-            <Fragment>
-              <NavbarToggler onClick={this.toggle} />
-              <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                  <NavItem>
-                    <NavLink tag={Link} to="/add">New Question</NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <User id={authedUser}/>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink onClick={this.logout}>Logout</NavLink>
-                  </NavItem>
-                </Nav>
-              </Collapse>
-            </Fragment>
-            }
-          </Navbar>
-        </div>
-    );
-  }
+        return (
+            <div>
+                <Navbar bg="primary" variant="dark" light expand="md">
+                    <NavbarBrand tag={Link} to="/">Would You Rather</NavbarBrand>
+                    { authedUser &&
+                    <Fragment>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/add">New Question</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/leaderboard">LeaderBoard</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <User id={authedUser}/>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink onClick={this.logout}>Logout</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Fragment>
+                    }
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 NavBar.propTypes = {
-  authedUser: PropTypes.string,
+    authedUser: PropTypes.string,
 };
 
 function mapStateToProps ({ authedUser }) {
-  return {
-    authedUser
-  }
+    return {
+        authedUser
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    logout: () => {
-      dispatch(unsetAuthedUser())
+    return {
+        logout: () => {
+            dispatch(unsetAuthedUser())
+        }
     }
-  }
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar))
